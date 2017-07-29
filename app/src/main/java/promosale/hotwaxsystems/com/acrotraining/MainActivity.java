9a95e44443b36ed6e24d4f3f66bf614d7b0fab09
Lalit.Kushwah <lalitkushwah52525@gmail.com>
2017-07-29 12:28:13 +0530

    Testing of staus and diff cmd

--- app/src/main/java/promosale/hotwaxsystems/com/acrotraining/MainActivity.java
+++ app/src/main/java/promosale/hotwaxsystems/com/acrotraining/MainActivity.java
@@ -85,8 +85,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+
+        //Hello
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
